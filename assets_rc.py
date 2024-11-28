9722fd1682418d999db19bfbf303c2e0bf7e95a1
rushionn <lucienwooo@gmail.com>
2024-11-28 14:28:45 +0800

    Update assets_rc.py

--- assets_rc.py
+++ assets_rc.py
@@ -5,22 +5,76 @@
 
 from PySide2 import QtCore
 
+#qt_resource_data = b"\
+#\x00\x00\x00\xba\
+#\x89\
+#PNG\x0d\x0a\x1a\x0a\x00\x00\x00\x0dIHDR\x00\
+#\x00\x00 \x00\x00\x00 \x08\x06\x00\x00\x00szz\xf4\
+#\x00\x00\x00\x04sBIT\x08\x08\x08\x08|\x08d\x88\
+#\x00\x00\x00qIDATX\x85\xed\xd6;\x0a\x800\
+#\x10E\xd1{\xc5\x8d\xb9r\x97\x16\x0b\xad$\x8a\x82:\
+#\x16o\xda\x84pB2\x1f\x81Fa\x8c\x9c\x08\x04Z\
+#{\xcf\xa72\xbcv\xfa\xc5\x08 \x80r\x80\xfc\xa2\x0e\
+#\x1c\xe4\xba\xfaX\x1d\xd0\xde]S\x07\x02\xd8>\xe1w\
+#a-`\x9fQ\xe9\x86\x01\x04\x10\x00\x5c(Dk\x1b\
+#-\x04\xdc\x1d\x07\x14\x98;\x0bS\x7f\x7f\xf9\x13\x04\x10\
+#@\xf9X\xbe\x00\xc9 \x14K\xc1<={\x00\x00\x00\
+#\x00IEND\xaeB`\x82\
+#"
+
 qt_resource_data = b"\
-\x00\x00\x00\xba\
-\x89\
-PNG\x0d\x0a\x1a\x0a\x00\x00\x00\x0dIHDR\x00\
-\x00\x00 \x00\x00\x00 \x08\x06\x00\x00\x00szz\xf4\
-\x00\x00\x00\x04sBIT\x08\x08\x08\x08|\x08d\x88\
-\x00\x00\x00qIDATX\x85\xed\xd6;\x0a\x800\
-\x10E\xd1{\xc5\x8d\xb9r\x97\x16\x0b\xad$\x8a\x82:\
-\x16o\xda\x84pB2\x1f\x81Fa\x8c\x9c\x08\x04Z\
-{\xcf\xa72\xbcv\xfa\xc5\x08 \x80r\x80\xfc\xa2\x0e\
-\x1c\xe4\xba\xfaX\x1d\xd0\xde]S\x07\x02\xd8>\xe1w\
-a-`\x9fQ\xe9\x86\x01\x04\x10\x00\x5c(Dk\x1b\
--\x04\xdc\x1d\x07\x14\x98;\x0bS\x7f\x7f\xf9\x13\x04\x10\
-@\xf9X\xbe\x00\xc9 \x14K\xc1<={\x00\x00\x00\
-\x00IEND\xaeB`\x82\
+iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAIGNIUk0A\
+AHomAACAhAAA+gAAAIDoAAB1MAAA6mAAADqYAAAXcJy6UTwAAAAGYktH\
+RAD/AP8A/6C9p5MAAAV+SURBVFjD7ZdbbFRVFIa/dWbawgxQbtOYtkxr\
+sRewnZa2XIKQEGmCCfFKJaGtIDwIYkFNQMVA5EEjMRrjJcYYjBBFEm94\
+i8TEmBqpxjbQQgtMy3RKtVAo0EJLKWV6zvJh6oTCnGnV6Iuu5ORk9tl\
+7/f9a/1p774H/usk/6Tzf560zQ/35qNDo7zRGtSgvZ7LmZXusvwO8ZXP\
+lHbk5U61P3nhFv9+5U3NzktRurnM4+CSr4s4FVDUGRcVQxLjSeLzD/Wf\
+A09PTJ3737ac/rl22ShLHJnLklxpETdv5w9KiOKS48G42r3ySisXzSPF\
+MdeVlTdV1a0o/Hg349u3bjYlj+7pmpmRJ8sQk/HX1+OYUI2KffWf0YYs\
+5s5ZSPMvkQO1+qg7WlfqyPfrO7r23zJtXcjY/K8kENUDQ66ros71voSI\
+cavXjqnaR6fGOSHo4AYm7EjIGXfGWk/qag6goc+fdg5tEjp1qpvLh8jM\
+F2R7KFs8nZDgxVLBEWbXpJQwJy2wOmrS1nGTX7rfZdeBLdsz1MYzlDXb\
+Tl9ycKfr6ph2IwpG6OjJ9uVztuUxLsBZ1DKA4cLrGsaJyOyhR+yg0eI1\
+T7R1cunCB55/fQk9fDw1NnVFZ2IrT3dVFXkEBZ35tR1WxcJCUkUdKZh7\
+lj23jQPXPnO48T3V19U1r20628uFHH7P52W08uHxlTAkcNw6ULiv75UB\
+tVcWM9Ew6TnfgTffS1hogs6iQO5Y8QMbthWzZ+hzv7txFw9EGCny5eL1\
+pGKpUff4FXB3gcvdFcgvymT17Fv19IYoLixnvjp9+1B/YN6IEADNnePS\
+1TTuQgQEaGuu4RjeL7i0jNceHIHR1dYV1FYvJkycDQlsgQKi3j/ofqil\
+avIgkrxd3ogvThLZAkN7ebp55bnPj4cbf8uyL8A9WCodrahAJYRoXQZS\
+qr/awfNqtJLgnDIFCsOkEF891gUDlhjUgIJbJu/v3snrVWoqL5pKRlUl\
+G5nRam1uwBq/kjioDAAXZHl1RMj/y+3j7WR5a/RQz832oIQSbAhiWsO6\
+JckSF5Phxw2vIHKBPB3jr1feZPiOT1qYAallUPr7Gqm86E5HetgitodZ\
+Rgc6efs529WIpBJtbCDYFEBUeeaKCac7xJMe5ESXyoDDJkYDXmMCGjQ8\
+R9DeTkX0bhjix1BqGab9FiUn14WauhpRjbacRDETMiESPbqwgzeEa6sT\
+hiZShOSqQGjeB9RtWETh+AolyIsQggA6Kk5pjLYgqpQ+uxjV2QuSzogg\
+jH3CmgCUgIpjGzQxsPRzxXzDaz3bEdD6as1xHmBQzBMFhiQhgqIiFdUM\
+OB0dIgAo4NDaJmC7uL3s04VTnOZ2RUyBZWT4k4in8nuaaFFXXSAAKye6\
+JGDpUF3+WwIIFs6ZMnZLKstJywht/JDbcrkT6DJPUcZNwquC8LkxDIU6\
+FW9yJDIiF25WIqiDWzRQcsQjU1/7Q+/KLb0g07ktKllLtr6O/+xLiNJj\
+iHs81yyRODNxxCcSPSaA/dA1N9rDt2RcQEY42H9Ge/r5Pjh4PRu4Xtuq\
+sLLtv210lSx5JS8lItc+xxaHan7Ecwr4PdoGAhRKHUDR/EalpaRQVzgc\
+Jw6x9vJxG//lhmLYEfDlJ1jtv7rFM04qZJURRhZ7e86iGZVIxcI8ZR3z\
+cGE53tJGc7OXprZX64ivvpS9cuPDX65c7bR0r4s1Ip/VEMCY+Gt6GEsd\
+7IkP9Vy9zprMd1CA1ZRqgXOrplhvBbQnk3Z40+M3XPyEj1IidjUlw403\
+NADVQTNZvfJgG/4WoBR91UAfVYYhBsLnlr+AjIuErIyA40HAHRW1Y2zZ\
+U1X/2X8uQ2dSAgUMMdBR7/cjZsLk4xiZgUbJkzr8Q//8GvwMcqxAQRdN\
+yvwAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyNC0xMS0yMVQwMzo1MzowMCs\
+wMDowML9GNDUAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjQtMTEtMjFUMDM\
+6NTM6MDArMDA6MDDOG4yJAAAAKHRFWHRkYXRlOnRpbWVzdGFtcAAyMDI\
+0LTExLTIxVDAzOjUzOjAwKzAwOjAwmQ6tVgAAAABJRU5ErkJggg==\
 "
+
+#qt_resource_name = b"\
+#\x00\x03\
+#\x00\x00v\xf3\
+#\x00p\
+#\x00i\x00c\
+#\x00\x0c\
+#\x06\x0d\xc0G\
+#\x00M\
+#\x00o\x00n\x00d\x00r\x00i\x00a\x00n\x00.\x00p\x00n\x00g\
+#"
 
 qt_resource_name = b"\
 \x00\x03\
@@ -32,6 +86,15 @@ qt_resource_name = b"\
 \x00M\
 \x00o\x00n\x00d\x00r\x00i\x00a\x00n\x00.\x00p\x00n\x00g\
 "
+
+#qt_resource_struct = b"\
+#\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+#\x00\x00\x00\x00\x00\x00\x00\x00\
+#\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+#\x00\x00\x00\x00\x00\x00\x00\x00\
+#\x00\x00\x00\x0c\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+#\x00\x00\x01\x80\x8e\x02\xff\x81\
+#"
 
 qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
